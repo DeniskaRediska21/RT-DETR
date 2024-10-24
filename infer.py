6ff3784bee0cb875eb80906a515449d193ac1d24
DeniskaRediska21 <lenis@lenis>
2024-10-24 13:11:26 +0300

    infer now also stores to mlflow

--- infer.py
+++ infer.py
@@ -1,19 +1,49 @@
 import torch
 import requests
-
+from pathlib import Path
 from PIL import Image
 from transformers import RTDetrForObjectDetection, RTDetrImageProcessor
+from transformers import pipeline
+import mlflow
+from mlflow.models import infer_signature
+import numpy as np
 
+
+DEVICE = 'cpu'
 url = 'http://images.cocodataset.org/val2017/000000039769.jpg' 
 image = Image.open(requests.get(url, stream=True).raw)
 
-image_processor = RTDetrImageProcessor.from_pretrained("PekingU/rtdetr_r50vd")
-model = RTDetrForObjectDetection.from_pretrained("PekingU/rtdetr_r50vd")
+PATH = Path('weights', 'RT_DETR_HF')
+image_processor = RTDetrImageProcessor.from_pretrained(PATH, local_files_only=True)
+model = RTDetrForObjectDetection.from_pretrained(PATH, local_files_only=True)
 
 inputs = image_processor(images=image, return_tensors="pt")
 
+model = model.to(DEVICE)
+inputs = inputs.to(DEVICE)
 with torch.no_grad():
     outputs = model(**inputs)
+
+mlflow.set_tracking_uri('http://localhost:5000')
+mlflow.set_experiment('LIZA')
+
+signature_DETR = infer_signature(
+        model_input={
+                    'images' : np.array(image),
+                    }, 
+        model_output = {k:np.array(v) for k,v in outputs.items() if not isinstance(v, list)}
+)
+
+model_name = 'LIZA-detector'
+
+pipe = pipeline(model = model.cpu(), image_processor=image_processor,device='cpu', task='object-detection')
+
+mlflow.transformers.log_model(
+    pipe, 
+    model_name, 
+    registered_model_name=model_name, 
+    signature = signature_DETR,
+)
 
 results = image_processor.post_process_object_detection(outputs, target_sizes=torch.tensor([image.size[::-1]]), threshold=0.3)
 
