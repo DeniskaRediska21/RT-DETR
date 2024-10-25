0f1bb853e926f49180851835c045882d030badbf
User <user@spbhws381-ubuntu.geoscan.lan>
2024-10-25 12:06:33 +0300

    change for server

--- config.py
+++ config.py
@@ -3,4 +3,19 @@ import os
 MLFLOW_URI = 'http://spbhws381-ubuntu.geoscan.lan:5000'
 PROJECT_NAME = 'LIZA'
 MODEL_NAME = 'LIZA-detector@base'
-DATASET_PATH = os.path.join('..', 'Dataset')
+# DATASET_PATH = os.path.join('..', 'Dataset')
+
+DATASET_PATH = os.path.join(
+'os.sep',
+'media',
+'taiga',
+'dfs',
+'taiga',
+'data',
+'N2Documents',
+'UD_g.makarov',
+'Датасет Аэрозрение без дубликатов ZIP v2',
+'02_second_part_DataSet_Human_Rescue',
+'02_second_part_DataSet_Human_Rescue',
+'obj',
+)
