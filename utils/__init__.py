96b0441442811c17a95a69083966ef1d298a73b3
Georgy Makarov <g.makarov@geoscan.ru>
2024-10-27 18:26:42 +0300

    Fix load_to_mlflow.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,1 @@
-from .load_to_mlflow import *
 from .model_preprocessing import *
--- utils/load_to_mlflow.py
+++ utils/load_to_mlflow.py
@@ -13,58 +13,59 @@ from config import (
     MODEL_NAME,
 )
 
-categories = ['person']
-id2label = {index: x for index, x in enumerate(categories, start=0)}
-label2id = {v: k for k, v in id2label.items()}
+if __name__ == '__main__':
+    categories = ['person']
+    id2label = {index: x for index, x in enumerate(categories, start=0)}
+    label2id = {v: k for k, v in id2label.items()}
 
-DEVICE = 'cpu'
-url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
-image = Image.open(requests.get(url, stream=True).raw)
+    DEVICE = 'cpu'
+    url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+    image = Image.open(requests.get(url, stream=True).raw)
 
-PATH = Path('weights', 'RT_DETR_HF')
-image_processor = RTDetrImageProcessor.from_pretrained(PATH, local_files_only=True)
-model = RTDetrForObjectDetection.from_pretrained(PATH,
-                                                 local_files_only=True,
-                                                 id2label=id2label,
-                                                 label2id=label2id,
-                                                 ignore_mismatched_sizes=True)
+    PATH = Path('weights', 'RT_DETR_HF')
+    image_processor = RTDetrImageProcessor.from_pretrained(PATH, local_files_only=True)
+    model = RTDetrForObjectDetection.from_pretrained(PATH,
+                                                     local_files_only=True,
+                                                     id2label=id2label,
+                                                     label2id=label2id,
+                                                     ignore_mismatched_sizes=True)
 
-inputs = image_processor(images=image, return_tensors="pt")
+    inputs = image_processor(images=image, return_tensors="pt")
 
-model = model.to(DEVICE)
-inputs = inputs.to(DEVICE)
-with torch.no_grad():
-    outputs = model(**inputs)
+    model = model.to(DEVICE)
+    inputs = inputs.to(DEVICE)
+    with torch.no_grad():
+        outputs = model(**inputs)
 
-mlflow_uri = MLFLOW_URI
-project_name = PROJECT_NAME
-model_name = MODEL_NAME.split('@')[0]
+    mlflow_uri = MLFLOW_URI
+    project_name = PROJECT_NAME
+    model_name = MODEL_NAME.split('@')[0]
 
-mlflow.set_tracking_uri(mlflow_uri)
-mlflow.set_experiment(project_name)
+    mlflow.set_tracking_uri(mlflow_uri)
+    mlflow.set_experiment(project_name)
 
-signature_DETR = infer_signature(model_input={
-    'images': np.array(image),
-},
-                                 model_output={
-                                     k: np.array(v) for k, v in outputs.items() if not isinstance(v, list)
-                                 })
+    signature_DETR = infer_signature(model_input={
+        'images': np.array(image),
+    },
+                                     model_output={
+                                         k: np.array(v) for k, v in outputs.items() if not isinstance(v, list)
+                                     })
 
-pipe = pipeline(model=model.cpu(), image_processor=image_processor, device='cpu', task='object-detection')
+    pipe = pipeline(model=model.cpu(), image_processor=image_processor, device='cpu', task='object-detection')
 
-mlflow.transformers.log_model(
-    pipe,
-    model_name,
-    registered_model_name=model_name,
-    signature=signature_DETR,
-)
+    mlflow.transformers.log_model(
+        pipe,
+        model_name,
+        registered_model_name=model_name,
+        signature=signature_DETR,
+    )
 
-results = image_processor.post_process_object_detection(outputs,
-                                                        target_sizes=torch.tensor([image.size[::-1]]),
-                                                        threshold=0.3)
+    results = image_processor.post_process_object_detection(outputs,
+                                                            target_sizes=torch.tensor([image.size[::-1]]),
+                                                            threshold=0.3)
 
-for result in results:
-    for score, label_id, box in zip(result["scores"], result["labels"], result["boxes"]):
-        score, label = score.item(), label_id.item()
-        box = [round(i, 2) for i in box.tolist()]
-        print(f"{model.config.id2label[label]}: {score:.2f} {box}")
+    for result in results:
+        for score, label_id, box in zip(result["scores"], result["labels"], result["boxes"]):
+            score, label = score.item(), label_id.item()
+            box = [round(i, 2) for i in box.tolist()]
+            print(f"{model.config.id2label[label]}: {score:.2f} {box}")
