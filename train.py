8cf3e98aeb69d35b3aba4863e3ec290e9ded03c4
Denis Gontar <d.gontar@geoscan.ru>
2024-10-29 16:58:15 +0300

    curr

--- train.py
+++ train.py
@@ -37,6 +37,9 @@ DEVICE = 'cuda'
 pipeline_ = get_model(mlflow_uri, project_name, model_name)
 model, image_processor = pipeline_.model, pipeline_.image_processor
 
+image_processor.do_resize = False
+image_processor.do_normalize = False
+
 dataset_path = DATASET_PATH
 dataset = LizaDataset(dataset_path, image_processor=image_processor, transforms=None)
 train_dataset, validation_dataset = torch.utils.data.random_split(dataset, lengths)
@@ -55,7 +58,7 @@ training_args = TrainingArguments(
     dataloader_num_workers=4,
     eval_strategy="epoch",
     save_strategy="steps",
-    save_steps=500,
+    save_steps=100,
     logging_steps=100,
     save_total_limit=2,
     remove_unused_columns=False,
--- utils/load_to_mlflow.py
+++ utils/load_to_mlflow.py
@@ -3,7 +3,7 @@ import os
 import requests
 from pathlib import Path
 from PIL import Image
-from transformers import RTDetrForObjectDetection, RTDetrImageProcessor, AutoImageProcessor, AutoModelForObjectDetection
+from transformers import RTDetrForObjectDetection, RTDetrImageProcessor, AutoImageProcessor, AutoModelForObjectDetection, DetrImageProcessor, DetrForObjectDetection
 from transformers import pipeline
 import mlflow
 from mlflow.models import infer_signature
@@ -19,19 +19,19 @@ from config import (
 
 
 def load_to_mlflow(PATH):
-    categories = ['N/A', 'person']
-    id2label = {index: x for index, x in enumerate(categories, start=0)}
-    label2id = {v: k for k, v in id2label.items()}
+    categories = ['person']
+    # id2label = {index: x for index, x in enumerate(categories, start=0)}
+    # label2id = {v: k for k, v in id2label.items()}
 
     DEVICE = 'cpu'
     url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
     image = Image.open(requests.get(url, stream=True).raw)
 
-    image_processor = AutoImageProcessor.from_pretrained(PATH, local_files_only=True)
-    model = AutoModelForObjectDetection.from_pretrained(PATH,
+    image_processor = DetrImageProcessor.from_pretrained(PATH, local_files_only=True)
+    model = DetrForObjectDetection.from_pretrained(PATH,
                                                      local_files_only=True,
-                                                     id2label=id2label,
-                                                     label2id=label2id,
+                                                     # id2label=id2label,
+                                                     # label2id=label2id,
                                                      ignore_mismatched_sizes=True)
 
     inputs = image_processor(images=image, return_tensors="pt")
@@ -84,7 +84,7 @@ def log_model(savedir):
 
 
 if __name__ == '__main__':
-    PATH = Path(os.sep, 'home', 'user', 'LIZA', 'RT-DETR', 'weights', 'RT_DETR_HF')
+    PATH = Path(os.sep, 'home', 'user', 'LIZA', 'RT-DETR', 'weights', 'RT_DETR_VISDRONE_HF')
     load_to_mlflow(PATH)
     # PATH = Path(os.sep,
     #             'home',
